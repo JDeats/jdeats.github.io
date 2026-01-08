c5f4cb1e61f765fde63acd2177be93711e169302
DEATSPC\JDeat <JDeaton26@gmail.com>
2026-01-07 17:51:20 -0800

    Restyled the Home Page a bit.

--- src/app/layout/App.tsx
+++ src/app/layout/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router';
-import './App.css'
 import NavBar from './NavBar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 import { Container } from 'react-bootstrap';
 
 function App() {
--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -1,8 +1,20 @@
 export default function HomePage() {
   return (
-    <>
-        <img src='./images/MeAtComputer.jpg' width="60%"/>
-        <h1>Hi, I'm Jared Deaton</h1>
-    </>
+    <div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6">
+            <h2>Welcome!</h2>
+            <img src='./images/MeAtComputer.jpg' width="100%"/>
+          </div>
+          <div className="col-6" style={{textAlign: "left", alignContent: "center"}}>
+            <h3 style={{marginBottom: "40px"}}>Hey, I'm Jared Deaton</h3>
+            <h4 style={{marginBottom: "0px"}}>C# Desktop App Developer <span style={{fontSize: "22px"}}>(3+ Years of Professional Experience)</span></h4>
+            <h4 style={{margin: "20px 0px"}}>Full Stack Web Developer Using React and ASP.NET Core</h4>
+            <h4>Embedded Hobbyist & Seasoned Electronics Technician</h4>
+          </div>
+        </div> 
+      </div>
+    </div>
   )
 }
